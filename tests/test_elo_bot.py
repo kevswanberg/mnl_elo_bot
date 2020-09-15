4707501c2acd61fcb6efafd02810c8158463849f
Kevan Swanberg <kevswanberg@gmail.com>
2020-09-15 18:53:10 -0400

    Simple test

--- tests/test_elo_bot.py
+++ tests/test_elo_bot.py
@@ -6,30 +6,16 @@ sys.path.append(".")
 from mnl_elo_bot import elo_bot  # noqa
 
 
-class TestTeam(unittest.TestCase):
-    def setUp(self):
-        self.team = elo_bot.Team("ponies", "blue")
-
-    def test_initialization(self):
-        self.assertEqual(self.team.name, "ponies")
-        self.assertEqual(self.team.color, "blue")
-        self.assertEqual(self.team.last_game_explanation(),
-                         "ponies rating is at 1500.0 (0.0)")
-
-    def test_win(self):
-        self.team.win(30)
-        self.assertEqual(self.team.history, [1500, 1530])
-        self.assertEqual(self.team.elo, 1530)
-        self.assertEqual(self.team.last_game_explanation(),
-                         "ponies rating goes up to 1530.0 (30.0)")
-
-    def test_lose(self):
-        self.team.lose(30)
-        self.assertEqual(self.team.history, [1500, 1470])
-        self.assertEqual(self.team.elo, 1470)
-        self.assertEqual(self.team.last_game_explanation(),
-                         "ponies rating goes down to 1470.0 (-30.0)")
-
+class IntegrationTest(unittest.TestCase):
+    # Just see if it initializes and runs...
+    def test_process(self):
+        elo_bot.process_results(elo_bot.get_raw_results_reader())
+        for team in elo_bot.TEAMS.values():
+            self.assertIsNotNone(team.name)
+            self.assertIsNotNone(team.color)
+            self.assertIsNotNone(team.emoji)
+            self.assertIsNotNone(team.history)
+    
 
 if __name__ == '__main__':
     unittest.main()
